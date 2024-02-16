2e01ca6d83c7554b28cb4a54980816d42c632ce3
unknown <salmanaidara01@gmail.com>
2024-02-16 15:36:19 +0000

    'misenpage'

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await  Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return  MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Tornadoes Driver 2024"),
+        ),
+        body: const Center(
+          child:  Text("Bienvenue chez Tornadoes Job Afrique"),
+        )
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-          await FirebaseFirestore.instance.collection('user').add({"id": "MonId"});
-         
-    
-      }),
     );
   }
 }
